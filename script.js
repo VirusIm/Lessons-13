4e2873a1061a140e067065a642630672bbea43ce
Virusim <marianna.12344475@gmail.com>
2021-12-02 03:18:09 +0200

    com

---
+++ script.js
@@ -0,0 +1,17 @@
+﻿/*
+function unique(arr) {
+    return Array.from(new Set(arr));
+  }
+*/
+
+function aclean(arr) {
+    let map = new Map();
+  
+    for (let word of arr) { //разбитие слов
+      let sorted = word.toLowerCase().split("").sort().join("");
+      map.set(sorted, word);
+    }
+    return Array.from(map.values());
+  }  
+  let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+  alert( aclean(arr) );
